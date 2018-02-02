bc4fb4b84f6055ed7d36b26c83531278ab8601cf
Matthieu Gourvénec <matthieu.gourvenec@gmail.com>
2018-02-02 12:01:54 +0100

    cours avant pause matib

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,12 +1,12 @@
 class JobsController < ApplicationController
 
+  before_action :find_job, only: [:show, :destroy]
+
   def index
     @jobs = Job.all
   end
 
-  def show
-    @job = Job.find(params[:id])
-  end
+  def show;end
 
   def new
     @job = Job.new
@@ -15,6 +15,16 @@ class JobsController < ApplicationController
   def create
     job = Job.create(params[:job].permit!)
     redirect_to root_path if job.persisted?
+  end
+
+  def destroy
+    @job.destroy
+  end
+
+  private
+
+  def find_job
+    @job = Job.find(params[:id])
   end
 
 end
--- db/migrate/20180126094857_add_age_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddAgeToUser < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :age, :integer, null: false, default: 0
-    # remove_column :users, :age
-    # change_column :users, :aage, :age
-  end
-end
